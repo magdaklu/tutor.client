b9e0fd8b728fce07b5ee06524873b68855e2f936
magdaklu <magda.e.klusak@gmail.com>
2022-10-05 20:05:57 +0200

    wheel form refactoring

--- src/tutor/excercises/WheelTask.jsx
+++ src/tutor/excercises/WheelTask.jsx
@@ -1,113 +1,28 @@
-import { arc, pie } from "d3-shape";
 import styles from "./WheelTask.module.css";
 import { useRef, useEffect } from "react";
-import { select } from "d3-selection";
+import {
+  getWheelConfig,
+  styleSvg,
+  cleanCurrentSvg,
+  getFormattedData,
+  addWheelTextPath,
+  addWheelSlices,
+  addWheelText,
+} from "./wheel";
 
 export const WheelTask = ({ wheel, setScore }) => {
   const svgRef = useRef();
-
   useEffect(() => {
-    const w = 250;
-    const h = w;
-    const slice = w / 10;
-    const sliceBreak = 3;
-    const scaleMax = 10;
-    const sliceStroke = "#70c7e6";
-    const sliceFill = "#fff";
-    const sliceOpacity = 0.7;
-    const sliceSelected = "#ffc107"; //"#6d4bb9";
+    const config = getWheelConfig();
+    const svg = styleSvg(svgRef.current, config.w, config.h);
+    cleanCurrentSvg(svg);
 
-    const svg = select(svgRef.current)
-      .attr("width", w)
-      .attr("height", h)
-      .style("overflow", "visible")
-      .style("margin-top", w + "px")
-      .style("margin-left", w + "px");
-
-    svg.selectAll("path,text").remove();
-
-    for (let step = 0; step < scaleMax; step++) {
-      let formatted = pie().value(1)(
-        wheel.map((x) => ({ id: x.id, label: x.label, step, score: x.score }))
-      );
-
-      if (step + 1 === scaleMax) {
-        svg
-          .selectAll()
-          .data(formatted)
-          .join("path")
-          .attr(
-            "d",
-            arc()
-              .innerRadius(step * slice)
-              .outerRadius((step + 1) * slice - sliceBreak)
-              .padAngle(0.6)
-              .padRadius(4)
-          )
-          .attr("fill", "none")
-          .each(function (d, i) {
-            var firstArcSection = /(^.+?)L/;
-            var newArc = firstArcSection.exec(select(this).attr("d"))[1];
-            newArc = newArc.replace(/,/g, " ");
-            svg
-              .append("path")
-              .attr("id", "hiddenArc" + step + i)
-              .attr("d", newArc)
-              .style("fill", "none");
-          });
-      }
-
-      svg
-        .selectAll()
-        .data(formatted)
-        .join("path")
-        .attr(
-          "d",
-          arc()
-            .innerRadius(step * slice)
-            .outerRadius((step + 1) * slice - sliceBreak)
-            .padAngle(0.6)
-            .padRadius(4)
-            .cornerRadius(6)
-        )
-        .attr("stroke", sliceStroke)
-        .attr("fill", (d) => {
-          if (d.data.score - 1 < d.data.step) {
-            return sliceFill;
-          }
-          return sliceSelected;
-        })
-        .attr("class", (d) => "index" + d.index + " step" + step)
-        .style("opacity", sliceOpacity)
-        .on("mouseover", function (d) {
-          select(this).attr("stroke", sliceSelected);
-        })
-        .on("click", handleChange)
-        .on("mouseout", function (d) {
-          select(this).attr("stroke", sliceStroke);
-        })
-        .attr("id", function (d, i) {
-          return "arc_" + step + i;
-        });
-
-      function handleChange(d, i) {
-        setScore(i);
-      }
+    for (let step = 0; step < config.scaleMax; step++) {
+      let data = getFormattedData(wheel, step);
+      addWheelTextPath(step, svg, data, config);
+      addWheelSlices(svg, data, step, config, (d, i) => setScore(i));
     }
-
-    svg
-      .selectAll()
-      .data(pie().value(1)(wheel))
-      .join("text")
-      .attr("x", 5)
-      .attr("dy", -10)
-      .append("textPath")
-      .attr("startOffset", "50%")
-      .style("text-anchor", "middle")
-      .attr("xlink:href", function (d, i) {
-        return "#hiddenArc" + 9 + i;
-      })
-      .text((d) => d.data.label);
+    addWheelText(svg, wheel);
   }, [wheel, setScore]);
 
   return (
---
+++ src/tutor/excercises/wheel.js
@@ -0,0 +1,120 @@
+import { select } from "d3-selection";
+import { arc, pie } from "d3-shape";
+
+export function getWheelConfig() {
+  const w = 250;
+  return {
+    w: w,
+    h: w,
+    sliceWidth: w / 10,
+    sliceBreak: 3,
+    scaleMax: 10,
+    sliceStroke: "#70c7e6",
+    sliceFill: "#fff",
+    sliceOpacity: 0.7,
+    sliceSelected: "#ffc107",
+  };
+}
+
+export function styleSvg(svgRef, width, height) {
+  return select(svgRef)
+    .attr("width", width)
+    .attr("height", height)
+    .style("overflow", "visible")
+    .style("margin-top", width + "px")
+    .style("margin-left", width + "px");
+}
+
+export function cleanCurrentSvg(svg) {
+  svg.selectAll("path,text").remove();
+}
+
+export function getFormattedData(wheel, step) {
+  return pie().value(1)(
+    wheel.map((x) => ({ label: x.label, step, score: x.score }))
+  );
+}
+
+export function addWheelTextPath(step, svg, data, config) {
+  if (step + 1 === config.scaleMax) {
+    svg
+      .selectAll()
+      .data(data)
+      .join("path")
+      .attr(
+        "d",
+        arc()
+          .innerRadius(step * config.sliceWidth)
+          .outerRadius((step + 1) * config.sliceWidth - config.sliceBreak)
+          .padAngle(0.6)
+          .padRadius(4)
+      )
+      .attr("fill", "none")
+      .each(function (d, i) {
+        const firstArcSection = /(^.+?)L/;
+        const shape = select(this).attr("d");
+        const firstArc = firstArcSection.exec(shape);
+        const newArc =
+          firstArc?.length > 0 ? firstArc[1].replace(/,/g, " ") : shape;
+        addHiddenPath(svg, newArc, i);
+      });
+  }
+}
+
+export function addHiddenPath(svg, shape, index) {
+  svg
+    .append("path")
+    .attr("id", "hiddenArc" + 9 + index)
+    .attr("d", shape)
+    .style("fill", "none");
+}
+export function addWheelSlices(svg, data, step, config, handleChange) {
+  svg
+    .selectAll()
+    .data(data)
+    .join("path")
+    .attr(
+      "d",
+      arc()
+        .innerRadius(step * config.sliceWidth)
+        .outerRadius((step + 1) * config.sliceWidth - config.sliceBreak)
+        .padAngle(0.6)
+        .padRadius(4)
+        .cornerRadius(6)
+    )
+    .attr("stroke", config.sliceStroke)
+    .attr("fill", (d) => {
+      if (d.data.score - 1 < d.data.step) {
+        return config.sliceFill;
+      }
+      return config.sliceSelected;
+    })
+    .attr("class", (d) => "index" + d.index + " step" + step)
+    .style("opacity", config.sliceOpacity)
+    .on("mouseover", function (d) {
+      select(this).attr("stroke", config.sliceSelected);
+    })
+    .on("click", handleChange)
+    .on("mouseout", function (d) {
+      select(this).attr("stroke", config.sliceStroke);
+    })
+    .attr("id", function (d, i) {
+      return "arc_" + step + i;
+    });
+}
+
+export function addWheelText(svg, data) {
+  svg
+    .selectAll()
+    .data(pie().value(1)(data))
+    .join("text")
+    .attr("x", 5)
+    .attr("dy", -10)
+    .append("textPath")
+    .attr("startOffset", "50%")
+    .style("text-anchor", "middle")
+    .attr("xlink:href", function (d, i) {
+      return "#hiddenArc" + 9 + i;
+    })
+    .text((d) => d.data.label);
+}
